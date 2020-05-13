5c16c9e34928d0b99673c5f86c7f2683d9bd3e5d
wahaj ahmed iqbal <wahaj465@gmail.com>
2020-05-13 18:32:51 +0500

    chages

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-const colorBtn = document.querySelector('.colorBtn');
-const body = document.querySelector('body');
+const hexNumber = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const hexBtn = document.querySelector('.hexbtn');
+const bodyBcg = document.querySelector('body');
+const hex = document.querySelector('.hex');
 
-const bgcolors = ['black' , 'green' ,'pink' , 'blue' , 'gray','purple','white','orange','red' ];
+hexBtn.addEventListener('click',getHex);
 
-colorBtn.addEventListener('click',changeColor);
+function getHex() {
+    var hexCol = '#';
 
-function changeColor() {
-    // body.style.backgroundColor = bgcolors;
-    var random = Math.floor(Math.random()*bgcolors.length)
-    body.style.backgroundColor = bgcolors[random];
+    for (let i = 0; i < 6; i++) {
+        var random = Math.floor(Math.random()*hexNumber.length);
+        hexCol += hexNumber[random];
+        
+    }
+
+    bodyBcg.style.backgroundColor = hexCol;
+    hex.innerHTML = hexCol;
+    
 }
